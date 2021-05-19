d4d08e3fdaeb293cbd56af693f7ca1031d7b6d5f
kenjidev <kenjidev@gmail.com>
2021-05-19 08:37:01 +0200

    Create OperationModeSmsConfig.php

---
+++ src/Model/OperationModeSmsConfig.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Easyblue YouSign project.
+ * (c) Easyblue <dev@easyblue.io>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Easyblue\YouSign\Model;
+
+use Symfony\Component\Serializer\Annotation\Groups;
+
+class OperationModeSmsConfig
+{
+    /**
+     * @Groups({"read", "write"})
+     */
+    protected ?string $content = null;
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(?string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+}
